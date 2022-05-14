710dc168e910f558b3e803b0e2a417b06edd3c72
Eman Odeh <emanodeh13821@gmail.com>
2022-05-14 04:05:07 -0700

    Created the User Management form.

--- TwoTier Architecture/Form1.Designer.cs
+++ TwoTier Architecture/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TwoTier_Architecture
 {
-    partial class Form1
+    partial class txtUserName
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblUserFullName = new System.Windows.Forms.Label();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.textUserName = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblUserFullName
+            // 
+            this.lblUserFullName.AutoSize = true;
+            this.lblUserFullName.Location = new System.Drawing.Point(39, 21);
+            this.lblUserFullName.Name = "lblUserFullName";
+            this.lblUserFullName.Size = new System.Drawing.Size(57, 13);
+            this.lblUserFullName.TabIndex = 0;
+            this.lblUserFullName.Text = "Full Name ";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(103, 21);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(475, 20);
+            this.txtFullName.TabIndex = 1;
+            // 
+            // textUserName
+            // 
+            this.textUserName.Location = new System.Drawing.Point(103, 61);
+            this.textUserName.Name = "textUserName";
+            this.textUserName.Size = new System.Drawing.Size(211, 20);
+            this.textUserName.TabIndex = 3;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(39, 64);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(60, 13);
+            this.lblUserName.TabIndex = 2;
+            this.lblUserName.Text = "User Name";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(103, 171);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(42, 233);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(612, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(103, 106);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(211, 20);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(39, 106);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtUserName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textUserName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.lblUserFullName);
+            this.Name = "txtUserName";
+            this.Text = "User Management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUserFullName;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox textUserName;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
     }
 }
 
--- TwoTier Architecture/Form1.cs
+++ TwoTier Architecture/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,25 @@ using System.Windows.Forms;
 
 namespace TwoTier_Architecture
 {
-    public partial class Form1 : Form
+    public partial class txtUserName : Form
     {
-        public Form1()
+        public txtUserName()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost; database=SoftwareArchitectureCourse; Integrated Secrity=true";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            connection.Close();
         }
     }
 }
