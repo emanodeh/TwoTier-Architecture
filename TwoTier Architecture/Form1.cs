b04c14e8cd5d4a5b628e307a28235d86d9ec3121
Eman Odeh <emanodeh13821@gmail.com>
2022-05-14 05:45:53 -0700

    Modify

--- TwoTier Architecture/Form1.cs
+++ TwoTier Architecture/Form1.cs
@@ -13,24 +13,44 @@ namespace TwoTier_Architecture
 {
     public partial class txtUserName : Form
     {
+        string connectionString = "Server=DESKTOP-7LUIOU2\MSSQLSERVER01; database=SoftwareArchitectureCourse; Integrated Security=true";
+        SqlConnection connection;
+
         public txtUserName()
         {
             InitializeComponent();
+             connection = new SqlConnection(connectionString);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            string command = "Select * from Users";
+
+            SqlCommand sqlCommand = new SqlCommand(command, connection);
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+            DataTable dt = new DataTable();
+
+            sqlDataAdapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
 
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            string connectionString = "Server=localhost; database=SoftwareArchitectureCourse; Integrated Secrity=true";
-            SqlConnection connection = new SqlConnection(connectionString);
+            string userFullName = txtFullName.Text;
+            string username = textUserName.Text;
+            string passwoed = txtPassword.Text;
+
+            string command = $"Insert into Users values('{userFullName}', '{username}', '{passwoed}')";
+            SqlCommand sqlCommand = new SqlCommand(command ,connection);
 
             if(connection.State != ConnectionState.Open)
                  connection.Open();
 
+            sqlCommand.ExecuteNonQuery();
 
             connection.Close();
         }
